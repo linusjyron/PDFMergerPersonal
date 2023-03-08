670b485a4c893e569c55ba50e3585271e0c5ea78
Linus Catubigan <linusjyron@gmail.com>
2023-03-08 20:42:32 +0800

    Multiple file merging. Mac, Linux, Windows support.

--- PDFMerger.py
+++ PDFMerger.py
@@ -2,9 +2,33 @@ import PyPDF2
 import sys
 import os
 
-merger = PyPDF2.PdfMerger()
+num_files = int(input("How many files do you want to merge? "))
+pdf_files = []
+for i in range(num_files):
+    file_name = input("Enter the name of file {}: ".format(i+1))
+    pdf_files.append(file_name)
 
-for file in os.listdir(os.curdir):
-	if file.endswith(".pdf"):
-		merger.append(file)
-	merger.write("combined.pdf")
+pdf_writer = PyPDF2.PdfWriter()
+
+for file_name in pdf_files:
+    with open(file_name, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            pdf_writer.add_page(page)
+
+merged_file_name = input("Enter the name of the merged file: ")
+with open(merged_file_name, 'wb') as merged_file:
+    pdf_writer.write(merged_file)
+    print("Merged file saved successfully!")
+
+if os.path.exists(merged_file_name):
+    if sys.platform.startswith('darwin'):
+        os.system("open " + merged_file_name)
+    elif os.name == 'nt':
+        os.startfile(merged_file_name)
+    elif os.name == 'posix':
+        os.system("xdg-open " + merged_file_name)
+else:
+    print("Error: Merged file was not created!")
